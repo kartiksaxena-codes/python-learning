a4b77202511250231ea1f97de12f2d5f783e4ca2
Kartik Saxena <saxenakartik261@gmail.com>
2026-09-12 19:49:46 +0530

    Add files via upload

---
+++ Py.Day 11.py
@@ -0,0 +1,15 @@
+#                      PATTERN FORMING            
+#      1 
+#      2  3
+#      4  5  6 
+
+
+
+
+
+c=1
+for line in range(1,4):
+    for i in range(line):
+        print(c,end=' ')
+        c=c+1
+    print()
